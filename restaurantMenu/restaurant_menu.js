6e98f5f864e135b8e3146150d96be6da12128f7b
bawfeh <bawfeh@gmail.com>
2025-07-30 20:07:46 -0400

    added restaurantMenu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+// populate breafast menu using the array method, map
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+// populate main course menu using the array method, forEach 
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+// populate the dessert menu using js for loop
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
